7699d98f4cb40687f2333fba42936e1583c354fa
Alexandr Zhuravlev <alexandr.zhuravlev@gmail.com>
2023-09-25 19:26:16 +0300

    ci(): Fix jest running on github

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  modulePathIgnorePatterns: [
+      '/dist/',
+  ],
   testMatch: [
     "**/tests/**/*.(spec|test).[tj]s"
   ]
